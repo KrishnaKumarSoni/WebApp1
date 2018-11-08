4cced58f30ffbd047cd1e3987df8935e259c7e54
KrishnaKumarSoni <39898943+KrishnaKumarSoni@users.noreply.github.com>
2018-11-08 21:09:30 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, flash, redirect, url_for, session, g
 import pymysql
 from datetime import datetime
+from datetime import timedelta
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 app.secret_key = 'my_key_is_my_key_none_of_your_key'
 
+photos=UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploadedimages'
+configure_uploads(app, photos)
 
 @app.route('/')
 def landing():
 	return redirect(url_for('login'))
 
+#----------------------------login---------------------------------------------
 @app.route('/login')
 def loginuser():
 	return render_template('login.html')
@@ -43,7 +50,7 @@ def login():
 				session['user'] = username
 				return redirect(url_for('homepage'))
 			else:
-				flash('you entered in the secret zone!')
+				flash('you entered wrong username/password')
 				return redirect(url_for('login'))		
 
 		else:
@@ -58,8 +65,10 @@ def before_request():
 
 @app.route('/logout')
 def logout():
+	session.pop('user', None)
 	return redirect(url_for('login'))
 
+#----------------------------signup---------------------------------------------
 @app.route('/signup')
 def signup():
 	return render_template('signup.html')
@@ -103,13 +112,14 @@ def registerUser():
 	else:
 		return "error"
 
-
+#----------------------------index-----------------------------------
 @app.route('/index')
 def index():
 	if g.user:
 		return render_template('index.html')
 	return redirect(url_for('loginuser'))
 
+#-------------------------------myaccount----------------------------------
 @app.route('/myaccount')
 def myaccount():
 	if g.user:
@@ -134,7 +144,69 @@ def myaccount():
 	else:
 		return redirect(url_for('login'))	
 
+#-------------------------------myads---------------------------------------------------
+@app.route('/myads', methods=['GET', 'POST'])
+def myads():
+	if g.user:
+		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
+								   charset='utf8mb4',
+								   cursorclass=pymysql.cursors.DictCursor)
+		cursor = connection.cursor()
+		query="SELECT * FROM solditems WHERE username='" + g.user + "'"
+		cursor.execute(query)
+		connection.commit()
+		result=cursor.fetchall()
 
+		if request.method=='POST':
+			primarykey=request.form['primarykey']
+			query1='DELETE FROM solditems WHERE primarykey=' + str(primarykey)
+			cursor.execute(query1)
+			connection.commit()
+
+			return redirect(url_for('myads'))
+
+		return render_template('myads.html', result=result)
+
+	else:
+		flash('please login first')
+		return redirect(url_for('login'))
+
+#------------------------------my cab shares----------------------------------------------
+@app.route('/mycabshares', methods=['GET', 'POST'])
+def mycabshares():
+	if g.user:
+		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
+								   charset='utf8mb4',
+								   cursorclass=pymysql.cursors.DictCursor)
+		cursor = connection.cursor()
+
+		query="SELECT * FROM cabsharing WHERE username='" + g.user + "'"
+		cursor.execute(query)
+		result=cursor.fetchall()
+		connection.commit()
+
+		if request.method=='POST':
+			if request.form['seats'].find('add') != -1:
+				print(request.form['seats'].find('add'))
+				cabnumber = request.form['seats'].split("_")
+				query2="UPDATE cabsharing SET numberofseats = numberofseats + 1 WHERE cabnumber='" + cabnumber[0] + "'"
+				cursor.execute(query2)
+				connection.commit()
+
+			elif request.form['seats'].find('subtract') != -1:
+				cabnumber = request.form['seats'].split("_")
+				query2="UPDATE cabsharing SET numberofseats = numberofseats - 1 WHERE cabnumber='" + cabnumber[0] + "'"
+				cursor.execute(query2)
+				connection.commit()
+			return redirect(url_for('mycabshares'))
+
+		return render_template('mycabshares.html', result=result)
+
+	else:
+		flash('login first')
+		return redirect(url_for('login'))
+
+#-----------------------------------edit account-----------------------------------------
 @app.route('/editaccounts', methods=['GET', 'POST'])
 def editaccounts():
 	if g.user:
@@ -162,28 +234,43 @@ def editaccounts():
 			return redirect(url_for('myaccount'))
 		return render_template('editaccounts.html', username=username, name=name)
 
-@app.route('/homepage')
+@app.route('/homepage', methods=['GET', 'POST'])
 def homepage():
-	return render_template('homepage.html')
+	if g.user:
+		if request.method=='POST':
+			catagory=request.form['catagoryselection']
+			if catagory != 'Category':
+				return redirect(url_for('feed', catagory=catagory))
+			else:
+				return redirect(url_for('homepage'))
+		return render_template('homepage.html')
+	else:
+		flash("please login")
+		return redirect(url_for('login'))
+
+#--------------------------------selling an item------------------------
 
 @app.route('/sellitem', methods=['GET', 'POST'])
 def sellitem():
 	if g.user:
-
-		if request.method=='POST':
+		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
+								   charset='utf8mb4',
+								   cursorclass=pymysql.cursors.DictCursor)
+		cursor = connection.cursor()
+		query = 'SELECT cities FROM city_dropdown'
+		cursor.execute(query)
+		cities = cursor.fetchall()
+		connection.commit()
+		
+		if request.method=='POST' and 'photo' in request.files:
 			username = g.user			
-			#getting user details
-			connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
-									   charset='utf8mb4',
-									   cursorclass=pymysql.cursors.DictCursor)
-			cursor = connection.cursor()
 			query = 'SELECT * FROM userinfo WHERE username=%s'
 			cursor.execute(query, (username))
 			useraccount=cursor.fetchmany(6)
 			connection.commit()
 
 			phonenumber = useraccount[0]['phonenumber'] 
-
+			name=useraccount[0]['name']
 			item=request.form
 			itemname=item['itemname']
 			itemdetails=item['itemdetails']
@@ -191,20 +278,67 @@ def sellitem():
 			city=item['city']
 			state=item['state']
 			catagory=item['catagory']
-			query1='INSERT INTO ' + str(catagory) + '(username, phonenumber, itemname, itemdetails, city, state, price) VALUES(%s, %s, %s, %s, %s, %s, %s)'
-			print(query1)
-			cursor.execute(query1, (username, phonenumber, itemname, itemdetails, city, state, price))
+				
+			filename=photos.save(request.files['photo'])
+			imagepath="uploadedimages/" + filename
+
+			query1='INSERT INTO solditems(username, name, phonenumber, itemname, itemdetails, city, state, price, catagory, imagepath) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+			cursor.execute(query1, (username, name, phonenumber, itemname, itemdetails, city, state, price, catagory, imagepath))
 			connection.commit()
 			connection.close()
 			return redirect(url_for('homepage'))
 
-		return render_template('sellitem.html')
+		return render_template('sellitem.html', cities=cities)
 
 
 	else:
 		return redirect(url_for('login'))
 
+#-------------------------------------item feed-------------------------------------------
+@app.route('/feed', methods=['GET', 'POST'])
+def feed():
+	if g.user:
+		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
+								   charset='utf8mb4',
+								   cursorclass=pymysql.cursors.DictCursor)
+		cursor = connection.cursor()
 
+		catagory=request.args.get('catagory')	
+		title=catagory
+		query="SELECT * FROM solditems WHERE catagory = '" + catagory.lower() +  "'"
+		cursor.execute(query)
+		connection.commit()
+		result = cursor.fetchall()
+
+		if request.method=='POST':
+			key=request.form['key']
+			return redirect(url_for('item', key=key))
+		return render_template('catagoryfeed.html', title=title, result=result)
+	else:
+		flash('please Login first')
+		return redirect(url_for('login'))
+
+#----------------------------------------item---------------------------------------------
+@app.route('/item')
+def item():
+	if g.user:
+		primarykey=request.args.get('key')
+		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
+								   charset='utf8mb4',
+								   cursorclass=pymysql.cursors.DictCursor)
+		cursor = connection.cursor()
+		query="SELECT * FROM solditems WHERE primarykey=" + str(primarykey)
+		cursor.execute(query)
+		connection.commit()
+		result=cursor.fetchall()
+
+		return render_template('item.html', result=result)
+		
+	else:
+		flash('please Login first')
+		return redirect(url_for('login'))
+
+#--------------------------create cabshare-----------------------------------------
 @app.route('/createcabshare', methods=['GET', 'POST'])
 def createcabshare():
 	if g.user:
@@ -217,22 +351,31 @@ def createcabshare():
 		cities = cursor.fetchall()
 		connection.commit()
 
+		query2 = "SELECT name, phonenumber FROM userinfo WHERE username='" + username + "'"
+		cursor.execute(query2)
+		userdetails=cursor.fetchall()
+		connection.commit()
+
 		if request.method=='POST':
 			journeydate=request.form['date']
-			time=request.form['time']
+			journeytime=request.form['time']
 			leavingfrom=request.form['leavingfrom']
 			goingto=request.form['goingto']
 			emptyseats=request.form['numberofseats']
-			
-			journeydatestring=datetime.strptime(journeydate, "%Y-%m-%d")
+			name=userdetails[0]['name']
+			phonenumber=userdetails[0]['phonenumber']
 
-			query1='INSERT INTO cabsharing(journeydate, goingto, numberofseats) VALUES ('+ +'"%s", %s, %s)'
-			cursor.execute(query1, (journeydatestring, goingto, emptyseats))
-			connection.commit()	
-			return redirect('homepage')
-		
+			if leavingfrom != goingto: 
+				query='INSERT INTO cabsharing(username, phonenumber, name, journeydate, journeytime, goingto, leavingfrom, numberofseats) VALUES(%s, %s, %s, %s, %s, %s)'
+				cursor.execute(query, (g.user, name, phonenumber, journeydate, journeytime, goingto, leavingfrom, emptyseats))
+				connection.commit()	
+				return redirect('homepage')
+			else:
+				flash("source and destination can't be same")
+				return redirect(url_for('createcabshare'))
 		return render_template('createcabshare.html', cities=cities)
 
+#-------------------------------------search a cab----------------------------------------
 @app.route('/cabsharesearch', methods=['GET', 'POST'])
 def cabsharesearch():
 	if g.user:
@@ -250,35 +393,40 @@ def cabsharesearch():
 			time=request.form['time']
 			leavingfrom=request.form['leavingfrom']
 			goingto=request.form['goingto']
+			
+			if leavingfrom != goingto:
+				flash("source and destination can't be same")
 
-			query1='SELECT * FROM cabsharing WHERE journeydate=' + journeydate
-			cursor.execute(query1)
-			connection.commit()
-			result = cursor.fetchall()
-			print(result)
+				query1="SELECT * FROM cabsharing WHERE journeydate='" + journeydate + "'"
+				cursor.execute(query1)
+				connection.commit()
+				result = cursor.fetchall()
 
+				print(result)
+				result_final=[]
+				for i in result:
+					journeytime = i['journeytime']
+					journeytime = str(journeytime)
+					journeytime_time = datetime.strptime(journeydate+ ' ' +journeytime, '%Y-%m-%d %H:%M:%S')
+					time_time = datetime.strptime(journeydate+ ' ' + time , '%Y-%m-%d %H:%M')
+					timediff = abs(journeytime_time - time_time)
+				
+					if timediff.seconds <= 3600:
+						result_final.append(i)
+
+				if result:
+					return render_template('searchresult.html', result=result_final)
+				else: 
+					flash('No cabs available. Make a share yourself!')
+					return redirect(url_for('createcabshare'))
+			else: 
+				flash("source and destination can't be same")
+				return redirect(url_for('cabsharesearch'))
 		return render_template('cabsharing.html', cities=cities)
 
 	else:
 		return redirect(url_for('login'))
 
-@app.route('/vehicles')
-def vehicles():
-	if g.user:
-		connection = pymysql.connect(host="localhost", user="root", password="", db='abcd',
-								   charset='utf8mb4',
-								   cursorclass=pymysql.cursors.DictCursor)
-		cursor = connection.cursor()
-		
-		query='SELECT COUNT(*) FROM vehicles'
-		cursor.execute("SELECT COUNT(*) FROM vehicles")
-		connection.commit()
-		result=cursor.fetchone()
-		print(result[0])
-		return "1"
-
-
-	else:
-		return redirect('login')	
+ 
 if __name__ == "__main__":
 	app.run(debug=True)			 
